37420b975b07e2eff36aa7e2332dd9da30c147a9
Javier Valencia <javier.valencia@app4u.com>
2023-03-26 18:18:08 -0400

    add environment variable to config file

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  url: "mongodb://127.0.0.1:27017/vidly",
-  jwtPrivateKey: "unsecureKey",
+  url: process.env.vidly_db
+    ? process.env.vidly_db
+    : "mongodb://127.0.0.1:27017/vidly",
+  jwtPrivateKey: process.env.jwtPrivateKey
+    ? process.env.jwtPrivateKey
+    : "unsecureKey",
   requiresAuth: true,
 };
